f45edf5195f5a5ba7df8605b6c718eb34bdd3a84
tmassontru <tay.j.masson@gmail.com>
2017-03-24 18:16:18 -0700

    Adds some scripts

---
+++ VRClassroom/Assets/ClickButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            print(this.gameObject);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                print(hit.transform.name);
+
+                if(hit.transform.name.Equals("Cube (1)"))
+                {
+                    this.enabled = !this.enabled;
+                }
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+            }
+        }
+    }
+}
